3b8121d1f4b76749789e2bdee7c9e7f473c369b4
Toni Markovic <tmarkovic@pmfst.hr>
2021-12-15 21:27:27 +0100

    dodane forme

--- HotelRestoranProjekt/Gosti.Designer.cs
+++ HotelRestoranProjekt/Gosti.Designer.cs
@@ -97,6 +97,7 @@ namespace HotelRestoranProjekt
             this.button1.TabIndex = 33;
             this.button1.Text = "Dodaj";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
